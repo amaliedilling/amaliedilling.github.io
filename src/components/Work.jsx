8ff8c7679e42b4455c5692a5129e5737718da9df
amaliedilling <apdilling@gmail.com>
2024-09-24 21:02:18 +0200

    work section

---
+++ src/components/Work.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { DiVisualstudio } from 'react-icons/di'
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen bg-[#F7F2EB] text-[#081F5C]'>
+        <div>
+            <div>
+                <p className='text-4xl font-bold inline border-b-4 border-[#081F5C]'>Work</p>
+                <p className='py-4'>// Here are some of my recent projects</p>
+            </div>
+
+            <div>
+                <div>
+                    <div>
+                        <span>
+
+                        </span>
+                        <div>
+                            <a href="/">
+                            <button></button>
+                            </a>
+                            <a href="/">
+                            <button></button> 
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Work
